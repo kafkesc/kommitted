0dd64fccb202a75cded25aaaf7315c5325f44494
Ivan De Marino <ivan.de.marino@gmail.com>
2023-03-12 18:48:26 +0000

    Adding `partition_offsets::init()` function to initialize module

--- src/partition_offsets/mod.rs
+++ src/partition_offsets/mod.rs
@@ -1,6 +1,37 @@
+// Inner modules
 mod emitter;
-mod lag_estimator;
-mod known_offset;
 mod errors;
+mod known_offset;
+mod lag_estimator;
+mod register;
 
-pub use emitter::{PartitionOffsets, PartitionOffsetsEmitter};
+// Exports
+pub use emitter::PartitionOffsetsEmitter;
+pub use errors::PartitionOffsetsError;
+pub use register::PartitionOffsetsRegister;
+
+// Imports
+use std::sync::Arc;
+
+use rdkafka::ClientConfig;
+use tokio::sync::broadcast;
+use tokio::task::JoinHandle;
+
+use crate::cluster_status::ClusterStatusRegister;
+use crate::internals::{Emitter, Register};
+
+pub fn init(
+    admin_client_config: ClientConfig,
+    cluster_status_register: Arc<ClusterStatusRegister>,
+    shutdown_rx: broadcast::Receiver<()>,
+) -> (PartitionOffsetsRegister, JoinHandle<()>) {
+    let (po_rx, poe_join) = PartitionOffsetsEmitter::new(
+        admin_client_config,
+        cluster_status_register,
+    )
+    .spawn(shutdown_rx);
+    let po_reg = PartitionOffsetsRegister::new(po_rx);
+
+    debug!("Initialized");
+    (po_reg, poe_join)
+}
