60bb3594fae089cad93bc713aacf3a46d929f2b2
Ivan De Marino <ivan.de.marino@gmail.com>
2023-07-01 17:26:32 +0100

    Adding utility method to `TopicPartition` to be able to create a vector of it, from a `TopicPartitions`
    It essentially denormalizes it.

--- src/kafka_types/topic_partition.rs
+++ src/kafka_types/topic_partition.rs
@@ -1,3 +1,6 @@
+use konsumer_offsets::TopicPartitions;
+use std::fmt::{Display, Formatter};
+
 /// Represents a single Topic-Partition pair
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default, Hash)]
 pub struct TopicPartition {
@@ -11,5 +14,19 @@ impl TopicPartition {
             topic,
             partition,
         }
+    }
+
+    pub(crate) fn vec_from(topic_partitions: TopicPartitions) -> Vec<Self> {
+        topic_partitions
+            .partitions
+            .into_iter()
+            .map(|p| TopicPartition::new(topic_partitions.topic.clone(), p as u32))
+            .collect()
+    }
+}
+
+impl Display for TopicPartition {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}:{}", self.topic, self.partition)
     }
 }
