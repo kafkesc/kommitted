cef9cd971262bfa50b8087f7b5483416f73c0b6e
Ivan De Marino <ivan.de.marino@gmail.com>
2023-09-24 19:15:58 +0100

    Modified `partition_offsets` module `Emitter` to use a much shorter interval (and a much deeper channel)

--- src/partition_offsets/emitter.rs
+++ src/partition_offsets/emitter.rs
@@ -13,10 +13,10 @@ use tokio_util::sync::CancellationToken;
 use crate::cluster_status::ClusterStatusRegister;
 use crate::internals::Emitter;
 
-const CHANNEL_SIZE: usize = 1000;
+const CHANNEL_SIZE: usize = 10_000;
 
 const FETCH_TIMEOUT: Duration = Duration::from_secs(10);
-const FETCH_INTERVAL: Duration = Duration::from_millis(500);
+const FETCH_INTERVAL: Duration = Duration::from_millis(10);
 
 /// Offset information for a Topic Partition.
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default, Hash)]
