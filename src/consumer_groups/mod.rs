c571e945d07e678e55ae80052e75453c3e0148b0
Ivan De Marino <ivan.de.marino@gmail.com>
2023-11-04 22:47:15 +0000

    Adding metrics to the `consumer_groups` module (#91)
    * Adding `consumer_groups` emitter Cluster and Kommitted (internal) metrics
    
    * Removing references to issues addressed by this work
    
    * Documented new metrics in `METRICS.md`
    
    * Deps upgrade
    
    * Remove empty lines from Prometheus `/metrics` output

--- src/consumer_groups/mod.rs
+++ src/consumer_groups/mod.rs
@@ -1,6 +1,9 @@
 // Inner module
 mod emitter;
 
+use std::sync::Arc;
+
+use prometheus::Registry;
 use rdkafka::ClientConfig;
 use tokio::sync::mpsc::Receiver;
 use tokio::task::JoinHandle;
@@ -13,8 +16,9 @@ pub use emitter::{ConsumerGroups, ConsumerGroupsEmitter};
 pub fn init(
     admin_client_config: ClientConfig,
     shutdown_token: CancellationToken,
+    metrics: Arc<Registry>,
 ) -> (Receiver<ConsumerGroups>, JoinHandle<()>) {
-    let consumer_groups_emitter = ConsumerGroupsEmitter::new(admin_client_config);
+    let consumer_groups_emitter = ConsumerGroupsEmitter::new(admin_client_config, metrics);
     let (cg_rx, cg_join) = consumer_groups_emitter.spawn(shutdown_token);
 
     debug!("Initialized");
